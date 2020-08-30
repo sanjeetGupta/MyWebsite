a789d8869be013446cb6eb60554504c696d0f31b
sgupta <sgupta@cognitionip.com>
2020-08-30 16:25:28 +0530

    solved routing issue for component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'my-website';
-  page = 'home';
-
-  // tslint:disable-next-line:typedef
-  openPage( pageName ) {
-    this.page = pageName;
+  constructor(public routr: Router ) {
+    console.log('gag'+ routr.url);
   }
+
 }
